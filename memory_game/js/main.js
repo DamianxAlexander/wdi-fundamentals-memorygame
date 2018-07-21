723c6aedadd0decbba4bcaf5af6bc2156214bbfd
Damian Salgado <damian.salgado@Damians-MacBook-Pro.local>
2018-07-21 03:27:51 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,26 @@
 console.log("Up and running!")
 
 var cards = ["queen","queen","king","king"]
-
 var cardsInPlay = []
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if(cardsInPlay.length === 2){
+var checkForMatch = function(){
 	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!")
-	}
+		alert("You found a match!");
+	} 
 	else{
-		alert("Sorry, try again.")
+		alert("Sorry, try again.");
 	}
 }
+
+var flipCard = function(cardId){
+
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
