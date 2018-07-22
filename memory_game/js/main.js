a5f666b46910a3e602a2e635de7a0cfedf60ab68
Damian Salgado <damian.salgado@Damians-MacBook-Pro.local>
2018-07-22 01:58:37 -0400

    game logic changes

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,28 +1,27 @@
-console.log("Up and running!")
-
 var cards = [
-{
-rank: 'queen',
-suit: 'hearts',
-cardImage: 'images/queen-of-hearts.png'
-},
-{
-rank: 'queen',
-suit: 'diamonds',
-cardImage: 'images/queen-of-diamonds.png'
-},
-{
-rank: 'king',
-suit: 'hearts',
-cardImage: 'images/king-of-hearts.png'
-},
-{
-rank: 'king',
-suit: 'diamonds',
-cardImage: 'images/king-of-diamonds.png'
-}
-]
-var cardsInPlay = []
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: 'images/queen-of-hearts.png'
+	},
+	{
+		rank: 'queen',
+		suit: 'diamonds',
+		cardImage: 'images/queen-of-diamonds.png'
+	},
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: 'images/king-of-hearts.png'
+	},
+	{
+		rank: 'king',
+		suit: 'diamonds',
+		cardImage: 'images/king-of-diamonds.png'
+	}
+];
+
+var cardsInPlay = [];
 
 var checkForMatch = function(){
 	if(cardsInPlay[0] === cardsInPlay[1]){
@@ -31,19 +30,29 @@ var checkForMatch = function(){
 	else{
 		alert("Sorry, try again.");
 	}
-}
+};
 
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
+	var card = cards[cardId];
+	this.setAttribute('src', card.cardImage);
+	cardsInPlay.push(card.rank);
 
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+	if(cardsInPlay.length === 2){
+		checkForMatch();
+	}
+};
 
-	cardsInPlay.push(cards[cardId].rank);
+var createBoard = function(){
+	for (var i = 0; i <cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		var board = document.getElementById("game-board");
+		board.appendChild(cardElement);
+	}
+};
 
-	checkForMatch();
-}
-
-flipCard(0);
-flipCard(2);
+createBoard();
 
